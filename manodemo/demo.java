01c66444b2ebd33c48d6c6f40864f66d04ff23a4
huangwei <huangwei@pgyer.com>
2019-05-06 12:07:01 +0800

    first commit

---
+++ manodemo/demo.java
@@ -0,0 +1,15 @@
+public class demo {
+    private String name;
+
+    public demo(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
